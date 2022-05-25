2f89dbf0fe1f101d49b61f302e74546abe91e8b7
yoelnacho-riu <iarguello@riu.com>
2022-05-25 14:47:21 +0200

    8 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainHeaderComponent } from './layout/main-header/main-header.component';
+import { MainFooterComponent } from './layout/main-footer/main-footer.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent, MainHeaderComponent],
+  declarations: [AppComponent, MainHeaderComponent, MainFooterComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
