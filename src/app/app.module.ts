9cbff477f6e1763aa513c35b89a5c1f993c61713
yoelnacho-riu <iarguello@riu.com>
2022-05-25 10:44:45 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './layout/main-header/main-header.component';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MainHeaderComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/layout/main-header/main-header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-header',
+  templateUrl: './main-header.component.html',
+})
+export class MainHeaderComponent {
+  //
+}
